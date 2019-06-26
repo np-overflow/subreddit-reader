3f7a3d30a0fdb4b6e36534f7dbcb4828b15395dc
Joel Tio <joeltiojinhon@gmail.com>
2019-06-26 17:47:02 +0800

    Add livedata example

--- subreddit-reader/app/src/test/java/club/npoverflow/subredditreader/ExampleUnitTest.kt
+++ subreddit-reader/app/src/test/java/club/npoverflow/subredditreader/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package club.npoverflow.subredditreader
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.*
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.mockito.Mockito.mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +14,26 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Test
+    fun livedata_demo() {
+        // Mock lifecycle owner
+        val lifecycle = LifecycleRegistry(mock(LifecycleOwner::class.java))
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+
+        // Create MutableLiveData
+        val x = MutableLiveData(5)
+        x.observe({ lifecycle }) {
+            println("The new value: $it")
+        }
+
+        println("Start")
+        x.postValue(20)
+        println("End")
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
