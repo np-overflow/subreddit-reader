6feed20ccc70ff7b2c20903e1dd8bf218a999f9f
Joel Tio <joeltiojinhon@gmail.com>
2019-06-27 02:39:59 +0800

    Add method to fetch data into FeedViewModel

--- subreddit-reader/app/src/main/java/club/npoverflow/subredditreader/feed/FeedViewModel.kt
+++ subreddit-reader/app/src/main/java/club/npoverflow/subredditreader/feed/FeedViewModel.kt
@@ -1,6 +1,26 @@
 package club.npoverflow.subredditreader.feed
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import club.npoverflow.subredditreader.feed.data.Post
+import kotlinx.coroutines.*
 
-class FeedViewModel : ViewModel() {
+class FeedViewModel(
+    private val feedRepository: FeedRepository
+) : ViewModel() {
+    // Kotlin coroutine scope
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    val feedLiveData: MutableLiveData<List<Post>> = MutableLiveData()
+
+    fun setFeedSource(subreddit: String) {
+        // TODO: check if this works
+        viewModelJob.cancelChildren()
+
+        // TODO: Check if this works
+        uiScope.async {
+            feedLiveData.value = feedRepository.getFeed(subreddit)
+        }
+    }
 }
