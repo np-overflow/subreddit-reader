95ce670ee3df2def3ee082743176af8e7060e7c9
Joel Tio <joeltiojinhon@gmail.com>
2019-06-27 01:55:17 +0800

    Add getByteArray to http module

--- subreddit-reader/app/src/main/java/club/npoverflow/subredditreader/http/DefaultWebservice.kt
+++ subreddit-reader/app/src/main/java/club/npoverflow/subredditreader/http/DefaultWebservice.kt
@@ -6,4 +6,8 @@ class DefaultWebservice : Webservice {
     override fun get(url: String): String {
         return URL(url).readText()
     }
+
+    override fun getByteArray(url: String): ByteArray {
+        return URL(url).readBytes()
+    }
 }
--- subreddit-reader/app/src/main/java/club/npoverflow/subredditreader/http/Webservice.kt
+++ subreddit-reader/app/src/main/java/club/npoverflow/subredditreader/http/Webservice.kt
@@ -2,4 +2,6 @@ package club.npoverflow.subredditreader.http
 
 interface Webservice {
     fun get(url: String): String
+
+    fun getByteArray(url: String): ByteArray
 }
