2209e16d20f90ee8d124781a97a3687f3bdf7227
Joel Tio <joeltiojinhon@gmail.com>
2019-06-27 14:48:10 +0800

    Remove ViewModel from FeedFragment

--- subreddit-reader/app/src/main/java/club/npoverflow/subredditreader/feed/FeedFragment.kt
+++ subreddit-reader/app/src/main/java/club/npoverflow/subredditreader/feed/FeedFragment.kt
@@ -16,15 +16,6 @@ import club.npoverflow.subredditreader.feed.data.Post
 import kotlinx.android.synthetic.main.fragment_feed.*
 
 class FeedFragment : Fragment() {
-    private lateinit var feedViewModel: FeedViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val factory = FeedViewModelFactory(context!!)
-        feedViewModel = ViewModelProviders.of(this, factory).get(FeedViewModel::class.java)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_feed, container, false)
     }
@@ -44,27 +35,10 @@ class FeedFragment : Fragment() {
         val dividerItemDecoration = DividerItemDecoration(
             recycler_view_feed.context, viewManager.orientation)
 
-        // Set an observer on the Feed LiveData for when the data is ready
-        feedViewModel.feedLiveData.observe(this, Observer<List<Post>> {
-            viewAdapter.setPosts(it)
-        })
-
         recycler_view_feed.apply {
             layoutManager = viewManager
             adapter = viewAdapter
             addItemDecoration(dividerItemDecoration)
-        }
-
-        recycler_view_feed.adapter
-
-
-        // Bind the "display" button to change the data source
-        button_fetch.setOnClickListener {
-            feedViewModel.setFeedSource(edittext_subreddit.text.toString())
-        }
-        button_fetch.setOnLongClickListener {
-            feedViewModel.setFeedSource(edittext_subreddit.text.toString(),true)
-            true
         }
     }
 }
